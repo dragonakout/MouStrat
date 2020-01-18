b71ef613b8fd45b3d8eafe0e4cfbb3bd2ba23db5
dragonakout <maigan.periot@yahoo.ca>
2020-01-17 23:07:01 -0500

    added GameManager basics

---
+++ MouStrats/Assets/scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject levelManager;
+    [SerializeField] GameObject gameStateManager;
+    [SerializeField] GameObject mouseManager;
+
+    private List<GameObject> persistentObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitPersistentObjects();
+    }
+
+    void Update()
+    {
+        if (getCurrentScene().name == "MainMenu") {
+            return;
+        }
+        CheckPauseMenu();
+    }
+
+    private void InitPersistentObjects()
+    {
+        persistentObjects = new List<GameObject>();
+    }
+
+    private Scene getCurrentScene()
+    {
+        return SceneManager.GetActiveScene();
+    }
+
+
+    private void CheckPauseMenu() {
+    }
+}
